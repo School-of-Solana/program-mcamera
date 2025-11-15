a4ce7c6413568195c776541df65be13d1d84146c
Marcelo Camera <mcamera.dev@pm.me>
2025-11-15 20:40:20 -0300

    feat: add create a project feature with unit tests

--- fundingme_dapp/programs/fundingme_dapp/src/lib.rs
+++ fundingme_dapp/programs/fundingme_dapp/src/lib.rs
@@ -6,11 +6,52 @@ declare_id!("DmcSC8vFAoLr756aDoqkV13S6kosdHHNuziRezhCcKUi");
 pub mod fundingme_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_project(
+        ctx: Context<CreateProject>,
+        name: String,
+        financial_target: u64,
+    ) -> Result<()> {        
+        let project = &mut ctx.accounts.project;
+        project.owner = *ctx.accounts.user.key;
+        project.name = name;
+        project.financial_target = financial_target;
+        project.balance = 0;
+        project.status = "Active".to_string();
+        project.bump = ctx.bumps.project;
+
         msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Project Name: {}", project.name.to_string());
+        msg!("Project Owner pubkey: {}", project.key().to_string());
+        msg!("Project Data pubkey: {}", project.owner.key().to_string());
+        msg!("Financial Target: {}", project.financial_target.to_string());
+        msg!("Status: {}", project.status.to_string());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateProject<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 5000, //  8 + 2 + 4 + 200 + 1,
+        seeds = [b"project", user.key().as_ref()],
+        bump,
+    )]
+    pub project: Account<'info, ProjectAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct ProjectAccount {
+    owner: Pubkey,
+    name: String,
+    financial_target: u64,
+    balance: u64,
+    status: String,
+    bump: u8,
+}
--- fundingme_dapp/tests/fundingme_dapp.ts
+++ fundingme_dapp/tests/fundingme_dapp.ts
@@ -1,16 +1,45 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { FundingmeDapp } from "../target/types/fundingme_dapp";
+import * as assert from "assert";
 
-describe("fundingme_dapp", () => {
-  // Configure the client to use the local cluster.
+describe("Should initializate a project", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.fundingmeDapp as Program<FundingmeDapp>;
+  const user = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const [projectAccountPdaAddr] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("project"), user.publicKey.toBuffer()],
+      program.programId
+    );
+
+    // Airdrop SOL to the owner to pay for transaction fees
+    const airdropSignature = await anchor.getProvider().connection.requestAirdrop(
+      user.publicKey,
+      anchor.web3.LAMPORTS_PER_SOL
+    );
+    await anchor.getProvider().connection.confirmTransaction(airdropSignature);
+
+    // Call the create_project function with required parameters
+    const tx = await program.methods
+      .createProject("My Project", new anchor.BN(1000))
+      .accounts({
+        user: user.publicKey,
+        project: projectAccountPdaAddr,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([user])
+      .rpc({ commitment: "confirmed" });
+
+    // Fetch the created project account to verify
+    const projectAccount = await program.account.projectAccount.fetch(projectAccountPdaAddr);
+    assert.strictEqual(projectAccount.name, "My Project", "The name of the project should match to: My Project");
+    assert.strictEqual(projectAccount.balance.toNumber(), 0, "The project account balance should be initialized with 0");
+    assert.strictEqual(projectAccount.financialTarget.toNumber(), 1000, "The financeial target of the project should be initialized with 1000");
+
     console.log("Your transaction signature", tx);
+    console.log("Project created:", projectAccount);
   });
 });
